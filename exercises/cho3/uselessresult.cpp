56d2cd6ddd6d664cac0f639f38bdb588bd3f29a5
Liam <debevecliam@gmail.com>
2023-09-07 17:41:10 -0400

    not sure if its done correctly

---
+++ exercises/cho3/uselessresult.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+void print_twice(char phil) {
+    cout << phil << phil << endl;
+}
+
+
+int main() {
+    print_twice('z');
+    return 0;
+}

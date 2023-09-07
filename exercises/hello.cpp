26bc2d4b055b5b94f636f56cc0a00005c23156af
Liam <debevecliam@gmail.com>
2023-09-07 08:37:29 -0400

    reorganizing exercise files

---
+++ exercises/hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        int num = 42;
+
+
+        cout << "The meaning of life, the universe and everything is";
+        cout << " of course " << num << "." << endl;
+
+        return 0;
+}
+

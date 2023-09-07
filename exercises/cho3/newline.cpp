c2a2a201fe95f8a2f4af2ff099c261b71d743447
Liam <debevecliam@gmail.com>
2023-09-07 09:13:07 -0400

    the newline function will not not work when it is after main

---
+++ exercises/cho3/newline.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main()
+{
+    cout << "First Line." << endl;
+    new_line();
+    cout << "Second Line." << endl;
+    return 0;
+}
+
+void new_line()
+{
+    cout << endl;
+}
+
+#this will generate an error. How can you use a function in a program before you have laid out what that function is.

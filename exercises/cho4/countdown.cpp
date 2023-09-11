a9da61e15567dec77985691ff1275d4dd5cd9d55
Liam <debevecliam@gmail.com>
2023-09-11 09:24:10 -0400

    add compare.cpp

--- exercises/cho4/countdown.cpp
+++ exercises/cho4/countdown.cpp
@@ -3,3 +3,21 @@
 using namespace std;
 
 
+void countdown(int n) {
+    if (n == 0) {
+        cout << "Blastoff!" << endl;
+    } else {
+        cout << n << endl;
+        countdown(n-1);
+    }
+}
+
+
+int main()
+{
+    countdown(-2);
+    return 0;
+}
+
+
+//The function will keep going until the z shell fails. This is an oversight by the programmer. You might wanrt to put in a failsafe to implement this code so the user won't add a negative number.

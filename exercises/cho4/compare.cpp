e721f8964673d59fc1a370eb3c1a9b6242a4a18b
Liam <debevecliam@gmail.com>
2023-09-11 09:37:43 -0400

    finished compare.cpp

---
+++ exercises/cho4/compare.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+int x = 7;
+int y = 6;
+if (x > y) {
+    cout << "x is greater than y." << endl;
+} else if (x < y) {
+    cout << "x is less than y." << endl;
+} else if (x == y){
+    cout << "x and y are equal." << endl;
+
+} 
+
+
+
+
+}
+// works as it should for all 3 possibilities.

ae4696d105c4321012b3f03be3a23ac51af67760
Liam <debevecliam@gmail.com>
2023-09-17 19:31:47 -0400

    finished cho6

---
+++ exercises/cho6/table.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(){
+double x = 1.0;
+while (x < 66536.0) {
+    cout << x << "\t" << log(x) << "\n";
+    x = x + 1.0;
+}
+}

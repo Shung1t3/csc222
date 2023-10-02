ae6bd424e52122fd3ad38ed5678141a1cff17d94
Liam <debevecliam@gmail.com>
2023-10-02 09:55:56 -0400

    finished random.cpp

--- exercises/ch10/random.cpp
+++ exercises/ch10/random.cpp
@@ -1,5 +1,17 @@
 #include <iostream> 
 #include <math.h>
 #include <vector>
+#include <cstdlib>
 using namespace std;
+
+int main(){
+
+ cout << "RAND_MAX is: " << RAND_MAX << endl;
+
+ for (int i = 1; i < 11; i++) {
+  cout << "Random number " << i << ": " << rand() << endl;}
+
+return 0;
+
+}
 

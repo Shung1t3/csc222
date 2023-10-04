402782400947a1bdaa462f8a63f1c3fd92ebc5fd
Liam <debevecliam@gmail.com>
2023-10-04 16:02:43 -0400

    finished swaps.cpp

--- exercises/ch11/swaps.cpp
+++ exercises/ch11/swaps.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 using namespace std;
 
+void wont_swap(char a, char b)
+{
+    cout << "a: " << a << "  b: " << b << endl;
+    cout << "address of a: " << long(&a) << endl;
+    cout << "address of b: " << long(&b) << endl;
+    char temp = a;
+    a = b;
+    b = temp;
+    cout << "a: " << a << "  b: " << b << endl;
+}
 
+void will_swap(char& a, char& b)
+{
+    cout << "a: " << a << "  b: " << b << endl;
+    cout << "address of a: " << long(&a) << endl;
+    cout << "address of b: " << long(&b) << endl;
+    char temp = a;
+    a = b;
+    b = temp;
+    cout << "a: " << a << "  b: " << b << endl;
+
+}
 void will_swap_with_pointers(char* a, char* b)
 {
     cout << "a: " << *a << "  b: " << *b << endl;
@@ -13,12 +34,21 @@ void will_swap_with_pointers(char* a, char* b)
     cout << "a: " << *a << "  b: " << *b << endl;
 }
 
+
+
+
 int main(){
 char c = 'c';
 char d = 'd';
-cout << "Calling will_swap_with_pointers..." << endl;
+wont_swap(c, d);
+cout << c << d << endl;
+
 will_swap_with_pointers(&c, &d);
 
+
 cout << c << d << endl;
+will_swap(c, d);
+cout << c << d << endl;
+
 
 }
